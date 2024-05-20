1766d60516086ffce3fb5b00862257cefdb7414e
Erick-Brunner <erickbrunerhuebra@gmail.com>
2024-05-20 20:18:47 -0300

    estruturas

--- Projetos JavaScript/2_operadores/js/scripts.js
+++ Projetos JavaScript/2_operadores/js/scripts.js
@@ -1,74 +1,74 @@
-// // 1 - Number
-// console.log(typeof 2);
-// console.log(typeof 5.14);
-// console.log(typeof -127);
-// // 2- Operaçoes aritimeticas
-// console.log(2+4);
-// console.log(10-5); 
-// console.log(2*5);
-// console.log(4/8);
-// console.log(2+(42/2));
-// console.log(3*(1+2));
-// console.log(5*4);
-// console.log(10/2);
-// console.log(5+(4*2));
-// // 3- Especila numbers
-// console.log(typeof Infinity);
-// console.log(typeof -Infinity);
-// console.log(12* "asd");
-// console.log(typeof NaN);
-// // 4- Strings
-// console.log("Um texto");
-// console.log('Mais um texto');
-// console.log('13');
-// console.log(typeof "Um texto");
-// console.log(typeof 'Mais um texto');
-// // 5- Simbolos especials em string
-// console.log("Testando a \n quebra de linha");
-// console.log("Espaçamento \t de tab");
-// // 6- Concatenaçao
-// console.log("oi," + " tudo" + " bem?");
-// console.log('testando '+'com '+ 'crase!');
-// // 7- Templates Strings
-// console.log(`A soma de 2 + 2 é: ${2 + 2}`);
-// console.log(`Podemos executar qualquer coiusa aqui ${console.log("teste")}`);
-// // 8- Boolean
-// console.log(true);
-// console.log(5 > 20);
-// console.log(30 > 10);
-// console.log(typeof false);
-// // 9- Comparaões
-// console.log(5 <= 5);
-// console.log(5 < 5);
-// console.log(10 == 10);
-// console.log(10 == 9);
-// console.log(10 != 9);
-// // 10- Identico
-// console.log(9 == "9");
-// console.log(9 === "9");
-// console.log(9 != "9");
-// console.log(9 !== "9");
-// // 11- operadores logicos
-// console.log(5 > 2 && 2 < 10);
-// console.log(5 > 2 && "Erick" === 1);
-// console.log(5 > 2 || "erick" === 1);
-// console.log(5 < 2 || 5> 100);
-// console.log(!true);
-// console.log(!5 > 2);
-// console.log("tabela verdade");
-// console.log("AND = &&");
-// console.log(false && false);
-// console.log(false && true);
-// console.log(true && false);
-// console.log(true && true);
-// console.log("OR = ||");
-// console.log(false || false);
-// console.log(false || true);
-// console.log(true || false);
-// console.log(true || true);
-// console.log("Not = !");
-// console.log(!false);
-// console.log(!true);
+// 1 - Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -127);
+// 2- Operaçoes aritimeticas
+console.log(2+4);
+console.log(10-5); 
+console.log(2*5);
+console.log(4/8);
+console.log(2+(42/2));
+console.log(3*(1+2));
+console.log(5*4);
+console.log(10/2);
+console.log(5+(4*2));
+// 3- Especila numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log(12* "asd");
+console.log(typeof NaN);
+// 4- Strings
+console.log("Um texto");
+console.log('Mais um texto');
+console.log('13');
+console.log(typeof "Um texto");
+console.log(typeof 'Mais um texto');
+// 5- Simbolos especials em string
+console.log("Testando a \n quebra de linha");
+console.log("Espaçamento \t de tab");
+// 6- Concatenaçao
+console.log("oi," + " tudo" + " bem?");
+console.log('testando '+'com '+ 'crase!');
+// 7- Templates Strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+console.log(`Podemos executar qualquer coiusa aqui ${console.log("teste")}`);
+// 8- Boolean
+console.log(true);
+console.log(5 > 20);
+console.log(30 > 10);
+console.log(typeof false);
+// 9- Comparaões
+console.log(5 <= 5);
+console.log(5 < 5);
+console.log(10 == 10);
+console.log(10 == 9);
+console.log(10 != 9);
+// 10- Identico
+console.log(9 == "9");
+console.log(9 === "9");
+console.log(9 != "9");
+console.log(9 !== "9");
+// 11- operadores logicos
+console.log(5 > 2 && 2 < 10);
+console.log(5 > 2 && "Erick" === 1);
+console.log(5 > 2 || "erick" === 1);
+console.log(5 < 2 || 5> 100);
+console.log(!true);
+console.log(!5 > 2);
+console.log("tabela verdade");
+console.log("AND = &&");
+console.log(false && false);
+console.log(false && true);
+console.log(true && false);
+console.log(true && true);
+console.log("OR = ||");
+console.log(false || false);
+console.log(false || true);
+console.log(true || false);
+console.log(true || true);
+console.log("Not = !");
+console.log(!false);
+console.log(!true);
 // 12- Empyt Values
 console.log(typeof null, typeof undefined);
 console.log(null === undefined);
---
+++ Projetos JavaScript/3_Estruturas/js/scripts.js
@@ -0,0 +1,8 @@
+// 1- Variaveis
+let nome ="Erick";
+console.log(nome);
+nome = "Erick Bruner"
+console.log(nome);
+const idade = 23;
+console.log(idade);
+// idade = 32
