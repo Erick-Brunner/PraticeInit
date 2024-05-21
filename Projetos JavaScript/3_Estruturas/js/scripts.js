d9b52c7ab9a026b7a5df92cb438ff81daf4fa559
Erick-Brunner <erickbrunerhuebra@gmail.com>
2024-05-20 22:46:26 -0300

    seguencia de variaveis

--- Projetos JavaScript/3_Estruturas/js/scripts.js
+++ Projetos JavaScript/3_Estruturas/js/scripts.js
@@ -5,4 +5,18 @@ nome = "Erick Bruner"
 console.log(nome);
 const idade = 23;
 console.log(idade);
-// idade = 32
+// idade = 32; 
+console.log(typeof nome);
+console.log(typeof idade);
+// 2- mais sobre variaveis
+// let 2test = "invalido"
+// let @test = "invalido"
+let a=10, b=20, c=30;
+console.log(a,b,c);
+const nomecompleto = "Erick brunner"
+const nomeCompleto = "huebra neres"
+console.log(nomecompleto);
+console.log(nomeCompleto);
+let _test = "ok";
+let $test = "ok";
+console.log(_test, $test);
