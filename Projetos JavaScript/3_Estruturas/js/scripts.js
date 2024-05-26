727f3708276ea884b10c8dd22cebb18c5b03034b
Erick-Brunner <erickbrunerhuebra@gmail.com>
2024-05-26 20:45:03 -0300

    funcoes de math/console/alert/prompt

--- Projetos JavaScript/3_Estruturas/js/scripts.js
+++ Projetos JavaScript/3_Estruturas/js/scripts.js
@@ -20,3 +20,18 @@ console.log(nomeCompleto);
 let _test = "ok";
 let $test = "ok";
 console.log(_test, $test);
+// // 3- prompt
+// const age = prompt("Digite a sua idade:")
+// console.log(`Voce tem ${age} anos.`)
+// // 4- alert
+// alert("Testando")
+// const z = 10
+// alert(`O numero e ${z}`)
+// 5- Math
+console.log(Math.max(5,2,1,10));
+console.log(Math.floor(5.14));
+console.log(Math.ceil(5.14));
+// 5- console
+console.log("teste!");
+console.error("erro!");
+console.warn("aviso!");
