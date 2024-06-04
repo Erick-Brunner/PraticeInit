e43bdbbeb94b8d709361b5b87c0083928a0b27bf
Erick-Brunner <erickbrunerhuebra@gmail.com>
2024-06-04 16:27:52 -0300

    while/ infinit loop/ do while/ for/identação/ break/ continue/ switch/ erros de switch

--- Projetos JavaScript/3_Estruturas/js/scripts.js
+++ Projetos JavaScript/3_Estruturas/js/scripts.js
@@ -35,47 +35,126 @@
 // console.log("teste!");
 // console.error("erro!");
 // console.warn("aviso!");
-// 7- if
-const m = 10;
-if(m > 5){
-    console.log("M e maior que 5!")
-};
-const user = "João"
-if(user === "João"){
-    console.log("Bem-Vindo João!")
+// // 7- if
+// const m = 10;
+// if(m > 5){
+//     console.log("M e maior que 5!")
+// };
+// const user = "João"
+// if(user === "João"){
+//     console.log("Bem-Vindo João!")
+// }
+// if(user === "Maria"){
+//     console.log("Olá Maria!")
+// };
+// console.log(user === "João", user === "Maria")
+// // 8- else
+// const loggedIn = false;
+// if(loggedIn){
+//     console.log("Está autenticado!")
+// }else {
+//     console.log("Não está autenticado!")
+// }
+// const q = 10
+// const w = 15
+// if(q > 5 && w > 20) {
+//     console.log(" Números mais altos")
+// } else {
+//     console.log("Os números não são mais altos")
+// }
+// // 9- else if
+// if(1 > 2){
+//     console.log("Teste")
+// } else if(2 > 3) {
+//     console.log("Teste 2")
+// } else if (5 > 1) {
+//     console.log("Agora sim!")
+// }
+// const userName = "Erick"
+// const userAge = 23
+// if(userName === "José"){
+//     console.log("Bem vindo José")
+// } else if (userName === "Erick" && userAge === 23){
+//     console.log("Olá Erick, voce tem 23 anos!")
+// } else {
+//     console.log("Nenhuma condição aceita!")
+// }
+// 10- while 
+let p = 0
+while(p < 5){
+    console.log(`repetindo ${p}`)
+    p = p + 1;
 }
-if(user === "Maria"){
-    console.log("Olá Maria!")
-};
-console.log(user === "João", user === "Maria")
-// 8- else
-const loggedIn = false;
-if(loggedIn){
-    console.log("Está autenticado!")
-}else {
-    console.log("Não está autenticado!")
+// // loop infinito
+// let x = 10
+// while(x > 5){
+//     console.log(`Imprimindo ${x}`);
+// }
+// 11- do while
+let o = 10
+do{
+    console.log(`Valor de o: ${o}`)
+    o--;
+}while(o > 1);
+// 12- for
+for(let t = 0; t < 10; t++){
+    console.log("Repetindo algo...")
 }
-const q = 10
-const w = 15
-if(q > 5 && w > 20) {
-    console.log(" Números mais altos")
-} else {
-    console.log("Os números não são mais altos")
+let r = 10
+for(r;r > 0; r = r - 1){
+    console.log(`O r está diminuindo: ${r}`);
 }
-// 9- else if
-if(1 > 2){
-    console.log("Teste")
-} else if(2 > 3) {
-    console.log("Teste 2")
-} else if (5 > 1) {
-    console.log("Agora sim!")
+// 13- identação
+for(let u = 0; u < 10; u++){
+    if(u * 2 > 10){
+        console.log(`Maior que 10! ${u}`)
+    } else {
+        if(u / 2 === 0){
+            console.log("deu 0")
+        }
+    }
 }
-const userName = "Erick"
-const userAge = 23
-if(userName === "José"){
-    console.log("Bem vindo José")
-} else if (userName === "Erick" && userAge === 23){
-    console.log("Olá Erick, voce tem 23 anos!")
-} else {
-    console.log("Nenhuma condição aceita!")
+// 14- break
+for(let g = 20; g > 10 ; g--){
+    console.log(`O valor de g é : ${g}`)
+    if(g === 15) {
+        console.log(`Og é 15!`)
+        break;
+    }
+}
+// 15- continue
+for(let s = 0; s < 10; s = s + 1){
+    // operador resto = %
+    if(s % 2 === 0){
+        console.log("Númeor par!");
+        continue;
+    }
+    console.log(s);
+}
+// 16- switch
+const job = "Medico"
+switch(job){
+    case "Programador":
+        console.log("Voce é um Programador!")
+        break;
+    case "Advogado":
+        console.log("Voce é um Advogado!")
+        break;
+    case "Engenheiro":
+        console.log("Voce é um Engenehiro!")
+        break;
+        default:
+            console.log("Profissão não encontrada")
+}
+// switch "errado"
+const l = 100;
+switch(l){
+    case 200:
+        console.log("L é 200!");
+    case 100:
+        console.log("L é 100!");
+    case 10:
+        console.log("L é 10!");
+    default:
+        console.log("L não foi encontrado")
 }
