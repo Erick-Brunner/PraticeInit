741d2b1d6acff5e7769e1c60afe3f010f2801d86
Erick-Brunner <erickbrunerhuebra@gmail.com>
2024-06-21 14:28:59 -0300

    introdução a for of/ rest operator/ repeat/ join/ split/ padStart/ trim/ reverse/ includes/ forEach

--- Projetos JavaScript/5_ARRAYS e objetos/js/script.js
+++ Projetos JavaScript/5_ARRAYS e objetos/js/script.js
@@ -81,44 +81,114 @@
 // for(let i = 0; i < users.length; i++) {
 //     console.log(`Listando o usuário ${users[i]}`);
 // }
-// 11- push e pop
-const array = ["a", "b", "c"];
-array.push("d");
-console.log(array);
-console.log(array.length);
-array.pop();
-console.log(array);
-const itemRemovido = array.pop();
-console.log(itemRemovido);
-console.log(array);
-array.push("z", "x", "y");
-console.log(array);
-// 12- shift e undhift
-const letters = ["a", "b", "c"];
-const letter = letters.shift();
-console.log(letter);
-console.log(letters);
-letters.unshift("p", "q", "r");
-letters.unshift("z");
-console.log(letters);
-// 13- indexOf e lastIndexOf
-const myElements = ["Morango", "Maçã", "Abacate", "Pera", "Abacate"];
-console.log(myElements.indexOf("Maçã"));
-console.log(myElements.indexOf("Abacate"));
-console.log(myElements[2]);
-// identico
-console.log(myElements[myElements.indexOf("Abacate")]);
-console.log(myElements.lastIndexOf("Abacate"));
-console.log(myElements.indexOf("Mamão"));
-console.log(myElements.lastIndexOf("Mamão"));
-// 14- slice
-const testeSlice = ["a","b","c","d","e", "f"]
-const subArray = testeSlice.slice(2,4);
-console.log(subArray);
-console.log(testeSlice);
-const subArray2 = testeSlice.slice(2,4+1);
-console.log(subArray2);
-const subArray3 = testeSlice.slice(10,20);
-console.log(subArray3);
-const subArray4 = testeSlice.slice(2);
-console.log(subArray4);
+// // 11- push e pop
+// const array = ["a", "b", "c"];
+// array.push("d");
+// console.log(array);
+// console.log(array.length);
+// array.pop();
+// console.log(array);
+// const itemRemovido = array.pop();
+// console.log(itemRemovido);
+// console.log(array);
+// array.push("z", "x", "y");
+// console.log(array);
+// // 12- shift e undhift
+// const letters = ["a", "b", "c"];
+// const letter = letters.shift();
+// console.log(letter);
+// console.log(letters);
+// letters.unshift("p", "q", "r");
+// letters.unshift("z");
+// console.log(letters);
+// // 13- indexOf e lastIndexOf
+// const myElements = ["Morango", "Maçã", "Abacate", "Pera", "Abacate"];
+// console.log(myElements.indexOf("Maçã"));
+// console.log(myElements.indexOf("Abacate"));
+// console.log(myElements[2]);
+// // identico
+// console.log(myElements[myElements.indexOf("Abacate")]);
+// console.log(myElements.lastIndexOf("Abacate"));
+// console.log(myElements.indexOf("Mamão"));
+// console.log(myElements.lastIndexOf("Mamão"));
+// // 14- slice
+// const testeSlice = ["a","b","c","d","e", "f"]
+// const subArray = testeSlice.slice(2,4);
+// console.log(subArray);
+// console.log(testeSlice);
+// const subArray2 = testeSlice.slice(2,4+1);
+// console.log(subArray2);
+// const subArray3 = testeSlice.slice(10,20);
+// console.log(subArray3);
+// const subArray4 = testeSlice.slice(2);
+// console.log(subArray4);
+// 15- forEach
+const nums = [1, 2, 3, 4, 5]
+nums.forEach((numero) => {
+    console.log(`O número é ${numero}`);
+});
+const posts = [
+    {title: "Primerio post", category: "PHP"},
+    {title: "Segundo post", category: "JavaScript"},
+    {title: "Terceiro post", category: "Python"},
+];
+posts.forEach((post) =>{
+    console.log(`Exbindo post: ${post.title}, da categoria: ${post.category}`);
+});
+// 16- includes
+const brands = ["BMW", "VW", "Fiat"]
+console.log(brands.includes("Fiat"));
+console.log(brands.includes("KIA"));
+if(brands.includes("BMW")){
+    console.log("Há carros da marca BMW!");
+}
+// 17- reverse
+const reverseTest = [1, 2, 3, 4, 5]
+reverseTest.reverse();
+console.log(reverseTest);
+// 18- trim
+const trimTest = "    testando n"
+console.log(trimTest);
+console.log(trimTest.trim());
+console.log(trimTest.length);
+console.log(trimTest.trim().length);
+// 19- padStart
+const testePadStart = "1"
+const newNumber = testePadStart.padStart(4,"0");
+console.log(testePadStart);
+console.log(newNumber);
+const testePadEnd = newNumber.padEnd(10, "0");
+console.log(testePadEnd);
+// 20- split
+const frase = "O rato roeu a roupa do rei de Roma"
+const arraydafrase = frase.split(" ");
+console.log(arraydafrase);
+// 21- join
+const fraseDenovo = arraydafrase.join(" ");
+console.log(fraseDenovo);
+const itensParaComprar = ["Mause", "Teclado", "Monitor"]
+const fraseDeCompra = `Precisamos comprar: ${itensParaComprar.join(", ")}.`;
+console.log(fraseDeCompra);
+// 22- repeat
+const palavra = "Testando ";
+console.log(palavra.repeat(5));
+// 23- rest operator
+const somaInfinita = (...args) => {
+    let total = 0
+    for(let i = 0; i < args.length; i++) {
+        total += args[i];
+    }
+    return total;
+};
+console.log(somaInfinita(1, 2, 3));
+console.log(somaInfinita(1, 20, 34, 34343, 54645, 7568456, 3434, 6567, 8786));
+// 24- fot of
+const somaInfinita2 = (...args) => {
+    let total = 0;
+    for (num of args) {
+        total += num;
+    }
+    return total;
+}
+console.log(somaInfinita2(1, 2, 3));
+console.log(somaInfinita2(10, 20, 30));
